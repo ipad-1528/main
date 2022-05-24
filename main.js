c7afba13e99f3c9d77d67ec5405e83d10a3239c5
ipad-1528 <106132662+ipad-1528@users.noreply.github.com>
2022-05-25 08:21:27 +0900

    add canvas

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext(canvas);
+
+canvas.width  = 400;
+canvas.height = 300;
+
+let interval;
+
+canvas.onclick = () => {
+
+  canvas.onclick = null;
+}
