d2945318b314ed2cda1eda67a96f1a8c5aa5611a
ipad-1528 <106132662+ipad-1528@users.noreply.github.com>
2022-05-25 09:40:42 +0900

    update

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ const ctx = canvas.getContext(canvas);
 canvas.width  = 400;
 canvas.height = 300;
 
+/**
+ * mainloopdelay
+ * @type {number}
+ */
+const mainloopdelay = 1;
+
+/**
+ * mainloop interval
+ * @type {(number|undefined)}
+ */
 let interval;
 
 canvas.onclick = () => {
-
+  interval = setInterval(mainloop,mainloopdelay)
   canvas.onclick = null;
 }
